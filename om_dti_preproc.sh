287ce1b679d0fe74da81b978723070d25ab50fa0
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-07-15 13:15:10 -0400

    bugfix

--- om_dti_preproc.sh
+++ om_dti_preproc.sh
@@ -13,8 +13,8 @@ SUBJ=sub124
 HOMEDIR=/mindhive/evlab/Shared/diffusionzeynep/
 DIR=/mindhive/evlab/Shared/diffusionzeynep/DICOMS/sub124/dti/006/
 #this should have been run:unpacksdcmdir -src $PROJECT_DIR/DICOMS/$SUB/dicoms/ -targ $PROJECT_DIR/DICOMS/$SUB/ -run $dtirun dti nii dti.nii
-module add openmind/freesurfer/6.0.0
-module add openmind/fsl/5.0.6
+module add openmind/freesurfer
+module add openmind/fsl
 source /etc/profile.d/modules.sh
 mkdir -p $HOMEDIR/$SUBJ/dti/
 
