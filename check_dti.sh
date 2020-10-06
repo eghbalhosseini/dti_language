07bb9713f641697bea630c03befa3a70cf8c94a8
Ben <bglipkin@gmail.com>
2020-10-06 11:55:48 -0700

    added script to check images and list of missing files

---
+++ check_dti.sh
@@ -0,0 +1,9 @@
+for d in /mindhive/evlab/Shared/diffusionzeynep/sub*
+do
+	fname=$d/dti/dti_V1.nii.gz
+	if test -f $fname; then
+		freeview $fname:vector=true
+	else
+		: #echo $d | tee -a missing.txt
+	fi
+done
