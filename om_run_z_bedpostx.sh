3e27fb9da28f2fa34c43f8dbe557e0fafda8f9fb
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-10-13 10:47:53 -0400

    bugfix

--- om_run_z_bedpostx.sh
+++ om_run_z_bedpostx.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+module add openmind/freesurfer
+module add openmind/fsl/5.0.6
+
 SUBJ=sub129
 HOMEDIR=/mindhive/evlab/Shared/diffusionzeynep
 sbatch --array=0-100 -p normal --time=56:00:00 /mindhive/evlab/Shared/diffusionzeynep/scripts/Architract/z_run_bedpostX -h $HOMEDIR -s $SUBJ
