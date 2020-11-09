ab62623d5ef52b92dbd8d930358ffced85dcd053
Benjamin Lipkin <lipkinb@evnode.eth.cluster>
2020-11-09 18:21:19 -0500

    bug fixes

--- om_run_z_bedpostx.sh
+++ om_run_z_bedpostx.sh
@@ -3,7 +3,7 @@
 module add openmind/freesurfer
 module add openmind/fsl/5.0.6
 
-SUBJ=sub129
+SUBJ=sub007
 HOMEDIR=/mindhive/evlab/Shared/diffusionzeynep
 sbatch --array=0-100 -p normal --time=56:00:00 /mindhive/evlab/Shared/diffusionzeynep/scripts/Architract/z_run_bedpostX -h $HOMEDIR -s $SUBJ
 
--- om_run_z_bedpostx_all.sh
+++ om_run_z_bedpostx_all.sh
@@ -3,6 +3,9 @@
 module add openmind/freesurfer
 module add openmind/fsl/5.0.6
 
+module unload openmind/anaconda/2.5.0
+module load openmind/anaconda/1.9.2
+
 HOMEDIR=/mindhive/evlab/Shared/diffusionzeynep
 
 sb=0
@@ -11,9 +14,9 @@ se=20
 for i in {1..11}; do
 
 	n=0
-	for SUBJ in $HOMEDIR/sub*; do
+	for SUBJ in $HOMEDIR/sub?*; do
 		if [ "$n" -ge "$sb" ] && [ "$n" -lt "$se" ]; then
-			sbatch --array=0-100 -p normal --time=6:00:00 $ARCHITRACTDIR/z_run_bedpostX -h $HOMEDIR -s ${SUBJ##*/}
+			sbatch --array=0-100 -p normal --time=12:00:00 $ARCHITRACTDIR/z_run_bedpostX -h $HOMEDIR -s ${SUBJ##*/}
 		fi
 	n=$((n+1))
 	done
@@ -21,6 +24,6 @@ for i in {1..11}; do
 	sb=$((sb+20))
 	se=$((se+20))
 
-	sleep 6.1h
+	sleep 12.1h
 
 done
