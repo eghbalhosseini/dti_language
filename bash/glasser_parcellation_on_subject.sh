9eda904454f3c5260ddc3085c969014fd6c26815
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2021-12-29 11:28:57 -0500

    bugfix

---
+++ bash/glasser_parcellation_on_subject.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#SBATCH -c 8
+#SBATCH --exclude node[017-018]
+#SBATCH -t 5:00:00
+
+GRAND_FILE=$1
+OVERWRITE='false' # or 'true'
+#
+
+if [ -n "$SLURM_ARRAY_TASK_ID" ]; then
+  JID=$SLURM_ARRAY_TASK_ID    # Taking the task ID in a job array as an input parameter.
+else
+  JID=$2       # Taking the task ID as an input parameter.
+fi
+echo "${GRAND_FILE}"
+echo $JID
+
+while IFS=, read -r line_count subj_name sub_dti_dir fs_dir subj_glasser_txt glasser_rel_dir glasser_dest_dir ; do
+  #echo "line_count ${model}"
+  if [ $JID == $line_count ]
+    then
+      echo "found the right match ${line_count}"
+      run_subj_name=$subj_name
+      run_sub_dti_dir=$sub_dti_dir
+      run_fs_dir=$fs_dir
+      run_subj_glasser_txt=$subj_glasser_txt
+      run_glasser_rel_dir=$glasser_rel_dir
+      run_glasser_dest_dir=$glasser_dest_dir
+      do_run=true
+      break
+    else
+      do_run=false
+      #echo "didnt the right match"
+  fi
+
+done <"${GRAND_FILE}"
+
+echo "subj ${run_subj_name}"
+echo "dti_dir ${run_sub_dti_dir}"
+echo "fs_dir ${run_fs_dir}"
+echo "txt file ${run_subj_glasser_txt}"
+
+echo "glasser_relative ${run_glasser_rel_dir}"
+echo "glasser destination ${run_glasser_dest_dir}"
+
+
+
+#module add openmind/singularity
+#export SINGULARITY_CACHEDIR=/om/user/${USER}/st/
+#XDG_CACHE_HOME=/om/user/${USER}/st
+#export XDG_CACHE_HOME
+#
+## Get the relevant line from the parameters
+#singularity exec -B /om:/om,/mindhive:/mindhive /om/user/${USER}/simg_images/neural_manifolds_tiny.simg python /om/user/${USER}/neural_manifolds/mftma_capacity_analysis.py ${run_file} ${run_model} ${run_analyze} ${OVERWRITE}
+
--- bash/run_glasser_parcel_on_all_subjects.sh
+++ bash/run_glasser_parcel_on_all_subjects.sh
@@ -35,12 +35,12 @@ echo "looking at ${DTI_DIR} "
       fi
     done < <(find $DTI_DIR -type d -maxdepth 1 -name "sub*")
 
-#echo $LINE_COUNT
-#run_val=0
-#if [ "$LINE_COUNT" -gt "$run_val" ]; then
-#  echo "running  ${LINE_COUNT} "
-#   #nohup /cm/shared/admin/bin/submit-many-jobs $LINE_COUNT 150 200 50 manifold_capacity_script.sh $GRAND_MFTMA_FILE &
-#   nohup /cm/shared/admin/bin/submit-many-jobs 20 15 20 5 manifold_capacity_script.sh $GRAND_MFTMA_FILE &
-#  else
-#    echo $LINE_COUNT
-#fi
+echo $LINE_COUNT
+run_val=0
+if [ "$LINE_COUNT" -gt "$run_val" ]; then
+  echo "running  ${LINE_COUNT} "
+   #nohup /cm/shared/admin/bin/submit-many-jobs $LINE_COUNT 150 200 50 glasser_parcellation_on_subject.sh  $SUBJECT_GLASSER_FILE &
+   nohup /cm/shared/admin/bin/submit-many-jobs 20 15 20 5 glasser_parcellation_on_subject.sh  $SUBJECT_GLASSER_FILE &
+  else
+    echo $LINE_COUNT
+fi
