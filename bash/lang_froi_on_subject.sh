281b9418d5b303a0af4daf980e26b165ed725451
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-01-10 19:04:54 -0500

    setting up sub_fROI pipeline

---
+++ bash/lang_froi_on_subject.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#SBATCH -c 8
+#SBATCH --exclude node[017-018]
+#SBATCH -t 5:00:00
+
+GRAND_FILE=$1
+OVERWRITE='false' # or 'true'
+#
+
+if [ -n "$SLURM_ARRAY_TASK_ID" ]; then
+  JID=$SLURM_ARRAY_TASK_ID    # Taking the task ID in a job array as an input parameter.
+else
+  JID=$2       # Taking the task ID as an input parameter.
+fi
+echo "${GRAND_FILE}"
+echo $JID
+
+while IFS=, read -r line_count subj_name network_id threshold thr_type ; do
+  #echo "line_count ${model}"
+  if [ $JID == $line_count ]
+    then
+      echo "found the right match ${line_count}"
+      run_subj_name=$subj_name
+      run_network_id=$network_id
+      run_threshold=$threshold
+      run_thr_type=$thr_type
+      do_run=true
+      break
+    else
+      do_run=false
+  fi
+
+done <"${GRAND_FILE}"
+echo "subj:${run_subj_name}"
+echo "network:${run_network_id}"
+echo "threshold:${run_threshold}"
+echo "threshold_type :${run_thr_type}"
+echo $SUBJECTS_DIR
+
+
+. ~/.bash_profile
+conda activate dti_language
+echo $(which python)
+python /om/user/ehoseini/dti_language/find_sub_lang_fROI.py "$run_subj_name" "$run_network_id" "$run_threshold" "$run_thr_type"
+
+
+
+
---
+++ bash/run_lang_froi_on_all_subjects.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+DTI_DIR=/mindhive/evlab/Shared/diffusionzeynep/
+FS_DIR=/mindhive/evlab/u/Shared/SUBJECTS_FS/FS/
+
+
+network_id="lang"
+threshold="90"
+thr_type="top"
+#
+
+analyze_fROI='all_subject_for_fROI'
+i=0
+LINE_COUNT=0
+SUBJECT_FROI_FILE="${DTI_DIR}/${analyze_fROI}.txt"
+rm -f $SUBJECT_FROI_FILE
+touch $SUBJECT_FROI_FILE
+printf "%s,%s,%s,%s,%s\n" "row" "subject_name" "network_id" "threshold"  "thr_type"   >> $SUBJECT_FROI_FILE
+
+echo "looking at ${DTI_DIR} "
+SUBJ_LINE=0
+while read x; do
+      # check if file already exist in fmri dir
+      original=$DTI_DIR
+      correction=''
+      subject_name="${x/$original/$correction}"
+      possible_folder="${DTI_DIR}/${subject_name}/fmri"
+      possible_file="${possible_folder}/x.fsnative.${network_id}_roi_${thr_type}_${threshold}.nii.gz"
+      if [ -f "$possible_file" ]
+      then
+        true
+      else
+        echo "$possible_file dosent exists adding it"
+        LINE_COUNT=$(expr ${LINE_COUNT} + 1)
+        mkdir -p $possible_folder
+        printf "%d,%s,%s,%s,%s\n" "$LINE_COUNT" "$subject_name" "$network_id" "$threshold" "$thr_type"  >> $SUBJECT_FROI_FILE
+      fi
+done < <(find $DTI_DIR -type d -maxdepth 1 -name "sub*")
+
+echo $LINE_COUNT
+run_val=0
+if [ "$LINE_COUNT" -gt "$run_val" ]; then
+  echo "running  ${LINE_COUNT} "
+   #nohup /cm/shared/admin/bin/submit-many-jobs $LINE_COUNT 150 200 50 glasser_parcellation_on_subject.sh  $SUBJECT_GLASSER_FILE &
+   #nohup /cm/shared/admin/bin/submit-many-jobs $LINE_COUNT 100 150 50 lang_froi_on_subject.sh  $SUBJECT_FROI_FILE &
+  else
+    echo $LINE_COUNT
+fi
