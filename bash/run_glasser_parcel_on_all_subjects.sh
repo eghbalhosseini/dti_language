a38022ecb0feca853d2d441dff381367d8eaae4f
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-01-14 15:20:39 -0500

    bugfix

--- bash/run_glasser_parcel_on_all_subjects.sh
+++ bash/run_glasser_parcel_on_all_subjects.sh
@@ -53,8 +53,8 @@ echo $LINE_COUNT
 run_val=0
 if [ "$LINE_COUNT" -gt "$run_val" ]; then
   echo "running  ${LINE_COUNT} "
-   nohup /cm/shared/admin/bin/submit-many-jobs 3 2 3 1 glasser_parcellation_on_subject.sh  $SUBJECT_GLASSER_FILE
-   #nohup /cm/shared/admin/bin/submit-many-jobs $LINE_COUNT 100 120 20 glasser_parcellation_on_subject.sh  $SUBJECT_GLASSER_FILE &
+   #nohup /cm/shared/admin/bin/submit-many-jobs 3 2 3 1 glasser_parcellation_on_subject.sh  $SUBJECT_GLASSER_FILE
+   nohup /cm/shared/admin/bin/submit-many-jobs $LINE_COUNT 80 100 20 glasser_parcellation_on_subject.sh  $SUBJECT_GLASSER_FILE &
   else
     echo $LINE_COUNT
 fi
