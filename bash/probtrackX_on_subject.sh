5e294e1b3cdf5ca6b31b2428345db5e90c0cdeec
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-02-14 18:03:28 -0500

    script for running probX

---
+++ bash/probtrackX_on_subject.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+#SBATCH -c 8
+#SBATCH --exclude node[017-018]
+#SBATCH -t 5:00:00
+#SBATCH --mem=32G
+GRAND_FILE=$1
+OVERWRITE='false' # or 'true'
+#
+
+if [ -n "$SLURM_ARRAY_TASK_ID" ]; then
+  JID=$SLURM_ARRAY_TASK_ID    # Taking the task ID in a job array as an input parameter.
+else
+  JID=$2       # Taking the task ID as an input parameter.
+fi
+echo "${GRAND_FILE}"
+echo $JID
+
+while IFS=, read -r line_count subj_name ; do
+  #echo "line_count ${model}"
+  if [ $JID == $line_count ]
+    then
+      echo "found the right match ${line_count}"
+      SUB=$subj_name
+      do_run=true
+      break
+    else
+      do_run=false
+  fi
+
+done <"${GRAND_FILE}"
+echo "subj:${SUB}"
+
+
+. ~/.bash_profile
+. ~/.bashrc
+conda activate dti_language
+
+# step 1
+reg_file="/mindhive/evlab/Shared/diffusionzeynep/${SUB}/lang_glasser/reg_FS2nodif.dat"
+mov_file="/mindhive/evlab/Shared/diffusionzeynep/${SUB}/dti/nodif_brain.nii.gz"
+targ_file="/mindhive/evlab/Shared/diffusionzeynep/${SUB}/fs/mri/aparc+aseg.mgz"
+out_file="/mindhive/evlab/Shared/diffusionzeynep/${SUB}/indti/aparc+aseg-in-dti.nii.gz"
+mri_vol2vol --targ "${targ_file}" --reg "${reg_file}" --mov "${mov_file}" --inv --nearest --o "${out_file}"
+
+# step 2
+
+matlab -nosplash -nojvm -r "cd('/mindhive/evlab/Shared/diffusionzeynep/scripts/Architract/');\
+label_all_eh('${SUB}','/mindhive/evlab/Shared/diffusionzeynep/','aparc+aseg');exit"
+
+link_src="/mindhive/evlab/Shared/diffusionzeynep/${SUB}/indti/lang_glasser_LH_indti.nii.gz"
+link_targ="/mindhive/evlab/Shared/diffusionzeynep/${SUB}/indti/lang_glasser_LH-in-dti.nii.gz"
+ln -sf "${link_src}" "${link_targ}"
+
+link_src="/mindhive/evlab/Shared/diffusionzeynep/${SUB}/indti/lang_glasser_RH_indti.nii.gz"
+link_targ="/mindhive/evlab/Shared/diffusionzeynep/${SUB}/indti/lang_glasser_RH-in-dti.nii.gz"
+
+ln -sf "${link_src}" "${link_targ}"
+
+
+# step 3
+
+matlab -nosplash -nojvm -r "cd('/mindhive/evlab/Shared/diffusionzeynep/scripts/Architract/');\
+label_all_general('${SUB}','/mindhive/evlab/Shared/diffusionzeynep/','lang_glasser_LH','/mindhive/evlab/Shared/diffusionzeynep/FSLUT_lang_glasser/FSLUT_LH_lang_glasser_ctab.txt');\
+label_all_general('${SUB}','/mindhive/evlab/Shared/diffusionzeynep/','lang_glasser_RH','/mindhive/evlab/Shared/diffusionzeynep/FSLUT_lang_glasser/FSLUT_RH_lang_glasser_ctab.txt');\
+;exit"
+
+# step 4
+
+
+cp  "/mindhive/evlab/Shared/diffusionzeynep/${SUB}/indti/Labels/aparc+aseg/"all* "/mindhive/evlab/Shared/diffusionzeynep/${SUB}//indti/Labels/lang_glasser_LH/."
+cp  "/mindhive/evlab/Shared/diffusionzeynep/${SUB}//indti/Labels/aparc+aseg/"all* "/mindhive/evlab/Shared/diffusionzeynep/${SUB}//indti/Labels/lang_glasser_RH/."
+
+echo "${SUB} successfully processed!"
---
+++ bash/run_probtrackX_on_all_subjects.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+DTI_DIR=/mindhive/evlab/Shared/diffusionzeynep/
+FS_DIR=/mindhive/evlab/u/Shared/SUBJECTS_FS/FS/
+
+probtrackX_labels_='all_subject_probtrackX'
+i=0
+LINE_COUNT=0
+SUBJECT_PROBX_FILE="${DTI_DIR}/${probtrackX_labels_}.txt"
+rm -f $SUBJECT_PROBX_FILE
+touch $SUBJECT_PROBX_FILE
+printf "%s,%s,%s\n" "row" "subject_name" "segment_name"   >> $SUBJECT_PROBX_FILE
+
+echo "looking at ${DTI_DIR} "
+SUBJ_LINE=0
+while read x; do
+      # check if file already exist in labels dir
+      original=$DTI_DIR
+      correction=''
+      subject_name="${x/$original/$correction}"
+
+      lh_folder="${DTI_DIR}/${subject_name}/dti.probtrackx/lang_glasser_LH"
+      rh_folder="${DTI_DIR}/${subject_name}/dti.probtrackx/lang_glasser_RH"
+
+      if [ ! -d "$lh_folder" ]
+      then
+        printf "%d,%s,%s\n" "$LINE_COUNT" "$subject_name" "lang_glasser_LH" >> $UBJECT_PROBX_FILE
+        LINE_COUNT=$(expr ${LINE_COUNT} + 1)
+      fi
+      if [ ! -d "$rh_folder" ]
+      then
+        printf "%d,%s,%s\n" "$LINE_COUNT" "$subject_name" "lang_glasser_RH" >> $UBJECT_PROBX_FILE
+        LINE_COUNT=$(expr ${LINE_COUNT} + 1)
+      fi
+done < <(find $DTI_DIR -type d -maxdepth 1 -name "sub*")
+
+run_val=0
+if [ "$LINE_COUNT" -gt "$run_val" ]; then
+  echo "running  ${LINE_COUNT} jobs"
+   #nohup /cm/shared/admin/bin/submit-many-jobs 3 2 3 1 aparc_aseg_on_subject.sh  $SUBJECT_LABEL_FILE
+   #nohup /cm/shared/admin/bin/submit-many-jobs $LINE_COUNT 75 100 25 aparc_aseg_on_subject.sh  $SUBJECT_LABEL_FILE
+  else
+    echo $LINE_COUNT
+fi
