5d9fed9321ab9c1f0db7a3c879d78664d6cbb900
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-02-21 10:29:38 -0500

    running probtrackx

--- bash/test_label.sh
+++ bash/test_label.sh
@@ -3,7 +3,7 @@
 SUB=sub540
 mri_vol2vol --targ /mindhive/evlab/Shared/diffusionzeynep/$SUB/fs/mri/aparc+aseg.mgz --reg /mindhive/evlab/Shared/diffusionzeynep/$SUB/lang_glasser/reg_FS2nodif.dat --mov /mindhive/evlab/Shared/diffusionzeynep/$SUB/dti/nodif_brain.nii.gz --inv --nearest --o /mindhive/evlab/Shared/diffusionzeynep/$SUB/indti/aparc+aseg-in-dti.nii.gz
 
-matlab -nosplash -nojvm -r "cd('/mindhive/evlab/Shared/diffusionzeynep/scripts/Architract/');label_all('$SUB','/mindhive/evlab/Shared/diffusionzeynep/','aparc+aseg');exit"
+matlab -nosplash -nojvm -r "cd('/mindhive/evlab/Shared/diffusionzeynep/scripts/Architract/');label_all_eh('$SUB','/mindhive/evlab/Shared/diffusionzeynep/','aparc+aseg');exit"
 ln -s /mindhive/evlab/Shared/diffusionzeynep/$SUB/indti/lang_glasser_LH_indti.nii.gz /mindhive/evlab/Shared/diffusionzeynep//$SUB/indti/lang_glasser_LH-in-dti.nii.gz
 ln -s /mindhive/evlab/Shared/diffusionzeynep//$SUB/indti/lang_glasser_RH_indti.nii.gz /mindhive/evlab/Shared/diffusionzeynep//$SUB/indti/lang_glasser_RH-in-dti.nii.gz
 
