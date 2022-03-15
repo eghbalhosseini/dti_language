524065afa5303add07a2005daa66b8db91fa542e
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-03-15 10:52:48 -0400

    bugfix

--- bash/collect_probtrackX_result_on_subject.sh
+++ bash/collect_probtrackX_result_on_subject.sh
@@ -39,4 +39,5 @@ echo "hemi :${HEMI}"
 SUBJECT_SEGMENT_FILE="${DTI_DIR}/${SUB}/targets_lang_glasser_${HEMI}.txt"
 
 matlab -nosplash -nojvm -r "addpath('/om/user/ehoseini/dti_language/');\
+cd('/om/user/ehoseini/dti_language/');\
 transform_probtrackX_output('${FILE_NAME}','${SUBJECT_SEGMENT_FILE}','${SAVE_NAME}','${HEMI}');exit"
