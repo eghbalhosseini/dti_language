068981e7b9d95e7886fe62db61d88f0b8a28eb96
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-06-22 10:19:26 -0400

    trying select source targets

---
+++ bash/probtrackX_on_select_tracts.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+#SBATCH -c 8
+#SBATCH --exclude node[017-018,094]
+#SBATCH -t 96:00:00
+#SBATCH --mem=64G
+GRAND_FILE=$1
+OVERWRITE='false' # or 'true'
+#
+DTI_DIR=/mindhive/evlab/Shared/diffusionzeynep/
+if [ -n "$SLURM_ARRAY_TASK_ID" ]; then
+  JID=$SLURM_ARRAY_TASK_ID    # Taking the task ID in a job array as an input parameter.
+else
+  JID=$2       # Taking the task ID as an input parameter.
+fi
+echo "${GRAND_FILE}"
+echo $JID
+
+while IFS=, read -r line_count subj_name segment_name sourceJoin targetJoin hemi ; do
+  #echo "line_count ${model}"
+  if [ $JID == $line_count ]
+    then
+      echo "found the right match ${line_count}"
+      SUB=$subj_name
+      SEGNAME=$segment_name
+      HEMI=$hemi
+      SOURCES=$sourceJoin
+      TARGETS=$targetJoin
+      do_run=true
+      break
+    else
+      do_run=false
+  fi
+
+done <"${GRAND_FILE}"
+echo "subj:${SUB}"
+echo "segment :${SEGNAME}"
+echo "target : ${SOURCES}"
+echo "exclude : ${TARGETS}"
+echo "hemi :${HEMI}"
+
+# step 1 check if segment text files exist.
+#SUBJECT_SEGMENT_FILE="${DTI_DIR}/${SUB}/targets_${SEGNAME}_${TARGET}.txt"
+#SUBJECT_MASK_FILE="${DTI_DIR}/${SUB}/masks_${SEGNAME}_${TARGET}.txt"
+#
+#SEARCH_DIR=${DTI_DIR}/${SUB}/indti/Labels/${SEGNAME}
+#rm -f $SUBJECT_SEGMENT_FILE
+#rm -f $SUBJECT_MASK_FILE
+#
+#if [ ! -f "${SUBJECT_SEGMENT_FILE}" ]
+#      then
+#        touch $SUBJECT_SEGMENT_FILE
+#        while read x ; do
+#          if [[ $x == *"$EXLUDE"* ]]; then
+#           echo "excluding ${EXLUDE}!"
+#           else
+#             printf "%s\n" "${x}" >> $SUBJECT_SEGMENT_FILE
+#          fi
+#        done < <(find "${SEARCH_DIR}" -maxdepth 1 -type f -name "${HEMI}*" )
+#        # remove the target
+#        #line_to_rm=$(find "${SEARCH_DIR}" -maxdepth 1 -type f -name "${HEMI}_${TARGET}*")
+#        #echo $line_to_rm
+#        #sed "/^$line_to_rm/d" $SUBJECT_SEGMENT_FILE
+#      else
+#          true
+#fi
+#
+#if [ ! -f "${SUBJECT_MASK_FILE}" ]
+#      then
+#        touch $SUBJECT_MASK_FILE
+#        while read x ; do
+#          if [[ $x == *"$TARGET"* ]]; then
+#           echo "It's there!"
+#           printf "%s\n" "${x}" >> $SUBJECT_MASK_FILE
+#           else
+#             true
+#          fi
+#        done < <(find "${SEARCH_DIR}" -maxdepth 1 -type f -name "${HEMI}*" )
+#      else
+#          true
+#fi
+#
+#
+#probtrackx2 -x "${SUBJECT_SEGMENT_FILE}" \
+#  -l --pd -c  0.2 -S 2000 --steplength=0.5 -P 5000 --forcedir --opd \
+#  -s "${DTI_DIR}/${SUB}/dti.bedpostX/merged" \
+#  -m "${DTI_DIR}/${SUB}/indti/Labels/${SEGNAME}/all-whitematter+gray.nii.gz" \
+#  --dir="${DTI_DIR}/${SUB}/dti.probtrackx/${SEGNAME}_${TARGET}/" \
+#  --targetmasks="${DTI_DIR}/${SUB}/targets_lang_glasser_${HEMI}_${TARGET}.txt" \
+#  --wtstop="${DTI_DIR}/${SUB}/masks_lang_glasser_${HEMI}_${TARGET}.txt" \
+#  --network
+
---
+++ bash/run_probtrackX_on_select_tracts.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+DTI_DIR=/mindhive/evlab/Shared/diffusionzeynep/
+FS_DIR=/mindhive/evlab/u/Shared/SUBJECTS_FS/FS/
+
+
+probtrackX_labels_='all_subject_probtrackX_select_tracts'
+i=0
+LINE_COUNT=0
+SOURCE=("PostTemp_top_90" "AntTemp_top_90" "MFG_top_90")
+TARGETS=("IFG_top_90" "IFGorb_top_90")
+
+SOURCEJoin=$(IFS=- ; echo "${SOURCE[*]}")
+echo $SOURCEjoin
+
+TARGETSJoin=$(IFS=- ; echo "${TARGETS[*]}")
+echo $TARGETSJoin
+
+SUBJECT_PROBX_FILE="${DTI_DIR}/${probtrackX_labels_}.txt"
+rm -f $SUBJECT_PROBX_FILE
+touch $SUBJECT_PROBX_FILE
+printf "%s,%s,%s,%s,%s,%s\n" "row" "subject_name" "segment_name" "source_name" "target_name" "hemi"   >> $SUBJECT_PROBX_FILE
+
+echo "looking at ${DTI_DIR} "
+SUBJ_LINE=0
+while read x; do
+      # check if file already exist in labels dir
+      original=$DTI_DIR
+      correction=''
+      subject_name="${x/$original/$correction}"
+
+      lh_folder="${DTI_DIR}/${subject_name}/dti.probtrackx/lang_glasser_LH_${TARGETSJoin}/fdt_paths.nii.gz"
+      #rm $lh_folder
+
+      rh_folder="${DTI_DIR}/${subject_name}/dti.probtrackx/lang_glasser_RH_${TARGETSJoin}/fdt_paths.nii.gz"
+      #rm $rh_folder
+
+      if [ ! -f "$lh_folder" ]
+      then
+        LINE_COUNT=$(expr ${LINE_COUNT} + 1)
+        printf "%d,%s,%s,%s,%s,%s\n" "$LINE_COUNT" "$subject_name" "lang_glasser_LH" "$SOURCEJoin" "$TARGETSJoin" "LH" >> $SUBJECT_PROBX_FILE
+
+      fi
+      if [ ! -f "$rh_folder" ]
+      then
+        LINE_COUNT=$(expr ${LINE_COUNT} + 1)
+        printf "%d,%s,%s,%s,%s,%s\n" "$LINE_COUNT" "$subject_name" "lang_glasser_RH" "$SOURCEJoin" "$TARGETSJoin" "RH">> $SUBJECT_PROBX_FILE
+      fi
+done < <(find $DTI_DIR -maxdepth 1 -type d -name "sub*")
+
+run_val=0
+if [ "$LINE_COUNT" -gt "$run_val" ]; then
+  echo "running  ${LINE_COUNT} jobs"
+   nohup /cm/shared/admin/bin/submit-many-jobs 3 2 3 1 probtrackX_on_select_tracts.sh  $SUBJECT_PROBX_FILE
+   #nohup /cm/shared/admin/bin/submit-many-jobs $LINE_COUNT 75 100 25 probtrackX_on_ind_tracts.sh  $SUBJECT_PROBX_FILE &
+  else
+    echo $LINE_COUNT
+fi
