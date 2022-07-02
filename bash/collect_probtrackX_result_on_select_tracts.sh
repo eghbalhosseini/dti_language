54a985980acf0c869068b259d9826005fd279b20
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-07-02 19:19:10 -0400

    adding tract collector

---
+++ bash/collect_probtrackX_result_on_select_tracts.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#SBATCH -c 8
+#SBATCH --exclude node[017-018,094]
+#SBATCH -t 96:00:00
+#SBATCH --mem=64G
+GRAND_FILE=$1
+OVERWRITE='false' # or 'true'
+#
+DTI_DIR=/mindhive/evlab/Shared/diffusionzeynep/
+if [ -n "$SLURM_ARRAY_TASK_ID" ]; then
+  JID=$SLURM_ARRAY_TASK_ID    # Taking the task ID in a job array as an input parameter.
+else
+  JID=$2       # Taking the task ID as an input parameter.
+fi
+echo "${GRAND_FILE}"
+echo $JID
+
+while IFS=, read -r line_count subj_name hemi fdt_file save_file ; do
+  #echo "line_count ${model}"
+  if [ $JID == $line_count ]
+    then
+      echo "found the right match ${line_count}"
+      SUB=$subj_name
+      HEMI=$hemi
+      FILE_NAME=$fdt_file
+      SAVE_NAME=$save_file
+      do_run=true
+      break
+    else
+      do_run=false
+  fi
+
+done <"${GRAND_FILE}"
+echo "subj:${SUB}"
+echo "segment :${SEGNAME}"
+echo "hemi :${HEMI}"
+
+# step 1 check if segment text files exist.
+SUBJECT_SEGMENT_FILE="${DTI_DIR}/${SUB}/targets_lang_glasser_${HEMI}.txt"
+
+module load mit/matlab/2020b
+matlab -nosplash -nojvm -r "addpath('/om/user/ehoseini/dti_language/');\
+cd('/om/user/ehoseini/dti_language/');\
+transform_probtrackX_output('file_id','${FILE_NAME}','target_mask_file','${SUBJECT_SEGMENT_FILE}','save_dir','${SAVE_NAME}','hemi','${HEMI}');exit"
---
+++ bash/run_collect_probtrackX_result_on_select_tracts.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+DTI_DIR=/mindhive/evlab/Shared/diffusionzeynep/
+
+
+probtrackX_labels_='all_subject_collect_probtrackX_select_tracts_results'
+i=0
+LINE_COUNT=0
+SUBJECT_PROBX_FILE="${DTI_DIR}/${probtrackX_labels_}.txt"
+rm -f $SUBJECT_PROBX_FILE
+touch $SUBJECT_PROBX_FILE
+
+
+SOURCES=("IFG_top_90" "PostTemp_top_90")
+TARGETS=("IFG_top_90" "PostTemp_top_90")
+#EXCLUDES=("MFG_top_90" "IFGorb_top_90")
+#EXCLUDES=("IFGorb_top_90")
+EXCLUDES=("MFG_top_90")
+SOURCEJoin=$(IFS=- ; echo "${SOURCES[*]}")
+TARGETSJoin=$(IFS=- ; echo "${TARGETS[*]}")
+EXCLUDEJoin=$(IFS=- ; echo "${EXCLUDES[*]}")
+printf "%s,%s,%s,%s,%s\n" "row" "subject_name" "hemi" "file_loc" "save_loc"   >> $SUBJECT_PROBX_FILE
+
+echo "looking at ${DTI_DIR} "
+SUBJ_LINE=0
+while read x; do
+      # check if file already exist in labels dir
+      original=$DTI_DIR
+      correction=''
+      subject_name="${x/$original/$correction}"
+      lh_file="${DTI_DIR}/probtrackX_results_${SOURCEJoin}_TO_${TARGETSJoin}_EX_${EXCLUDEJoin}/${subject_name}_LH_fdt_network.mat"
+      rh_file="${DTI_DIR}/probtrackX_results_${SOURCEJoin}_TO_${TARGETSJoin}_EX_${EXCLUDEJoin}/${subject_name}_RH_fdt_network.mat"
+
+      if [ ! -f "$lh_file" ]
+      then
+        LINE_COUNT=$(expr ${LINE_COUNT} + 1)
+        # folder to find the file
+        lh_tr_file="${DTI_DIR}/${subject_name}/dti.probtrackx/lang_glasser_LH_${SOURCEJoin}_TO_${TARGETSJoin}_EX_${EXCLUDEJoin}/fdt_network_matrix"
+        printf "%d,%s,%s,%s,%s\n" "$LINE_COUNT" "$subject_name" "LH" "$lh_tr_file" "$lh_file" >> $SUBJECT_PROBX_FILE
+
+      fi
+      if [ ! -f "$rh_file" ]
+      then
+        LINE_COUNT=$(expr ${LINE_COUNT} + 1)
+        rh_tr_file="${DTI_DIR}/${subject_name}/dti.probtrackx/lang_glasser_RH_${SOURCEJoin}_TO_${TARGETSJoin}_EX_${EXCLUDEJoin}/fdt_network_matrix"
+        printf "%d,%s,%s,%s,%s\n" "$LINE_COUNT" "$subject_name" "RH" "$rh_tr_file" "$rh_file" >> $SUBJECT_PROBX_FILE
+      fi
+done < <(find $DTI_DIR -maxdepth 1 -type d -name "sub*")
+
+run_val=0
+if [ "$LINE_COUNT" -gt "$run_val" ]; then
+  echo "running  ${LINE_COUNT} jobs"
+   #nohup /cm/shared/admin/bin/submit-many-jobs 3 2 3 1 collect_probtrackX_result_on_subject.sh  $SUBJECT_PROBX_FILE
+   #nohup /cm/shared/admin/bin/submit-many-jobs $LINE_COUNT 75 100 25 collect_probtrackX_result_on_select_tracts.sh  $SUBJECT_PROBX_FILE &
+  else
+    echo $LINE_COUNT
+fi
