9c6736999e864d52e55f6f776dead7485d5ae3d8
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-08-02 16:46:33 -0400

    new settings for collect

--- bash/run_collect_probtrackX_result_on_select_tracts.sh
+++ bash/run_collect_probtrackX_result_on_select_tracts.sh
@@ -10,18 +10,18 @@ rm -f $SUBJECT_PROBX_FILE
 touch $SUBJECT_PROBX_FILE
 
 
-SOURCES=("IFGorb_top_${threshold}" "AntTemp_top_${threshold}")
-TARGETS=("IFGorb_top_${threshold}" "AntTemp_top_${threshold}")
-#EXCLUDES=("MFG_top_${threshold}" "IFG_top_${threshold}")
-EXCLUDES=("MFG_top_${threshold}")
-#EXCLUDES=("IFG_top_${threshold}")
-
-
-#SOURCES=("IFG_top_${threshold}" "PostTemp_top_${threshold}")
-#TARGETS=("IFG_top_${threshold}" "PostTemp_top_${threshold}")
-#EXCLUDES=("MFG_top_${threshold}" "IFGorb_top_${threshold}")
-#EXCLUDES=("IFGorb_top_${threshold}")
+#SOURCES=("IFGorb_top_${threshold}" "AntTemp_top_${threshold}")
+#TARGETS=("IFGorb_top_${threshold}" "AntTemp_top_${threshold}")
+#EXCLUDES=("MFG_top_90" "IFG_top_90")
 #EXCLUDES=("MFG_top_${threshold}")
+#EXCLUDES=("IFG_top_90")
+
+SOURCES=("IFG_top_${threshold}" "PostTemp_top_${threshold}")
+TARGETS=("IFG_top_${threshold}" "PostTemp_top_${threshold}")
+#EXCLUDES=("MFG_top_90" "IFGorb_top_90")
+#EXCLUDES=("IFGorb_top_90")
+EXCLUDES=("MFG_top_${threshold}")
+
 SOURCEJoin=$(IFS=- ; echo "${SOURCES[*]}")
 TARGETSJoin=$(IFS=- ; echo "${TARGETS[*]}")
 EXCLUDEJoin=$(IFS=- ; echo "${EXCLUDES[*]}")
@@ -31,6 +31,7 @@ printf "%s,%s,%s,%s,%s,%s\n" "row" "subject_name" "hemi" "file_loc" "segment_fil
 echo "looking at ${DTI_DIR} "
 SUBJ_LINE=0
 mkdir -p "${DTI_DIR}/probtrackX_results_${SOURCEJoin}_TO_${TARGETSJoin}_EX_${EXCLUDEJoin}"
+overwrite=false
 while read x; do
       # check if file already exist in labels dir
       original=$DTI_DIR
@@ -39,31 +40,57 @@ while read x; do
       lh_file="${DTI_DIR}/probtrackX_results_${SOURCEJoin}_TO_${TARGETSJoin}_EX_${EXCLUDEJoin}/${subject_name}_LH_fdt_network.mat"
       rh_file="${DTI_DIR}/probtrackX_results_${SOURCEJoin}_TO_${TARGETSJoin}_EX_${EXCLUDEJoin}/${subject_name}_RH_fdt_network.mat"
 
-      #if [ ! -f "$lh_file" ]
-      if [ true ]
+      if [ "$overwrite" = true ]
       then
+        echo "overwriting ${lh_file}"
         LINE_COUNT=$(expr ${LINE_COUNT} + 1)
         # folder to find the file
         lh_tr_file="${DTI_DIR}/${subject_name}/dti.probtrackx/lang_glasser_LH_thr_${threshold}_${SOURCEJoin}_TO_${TARGETSJoin}_EX_${EXCLUDEJoin}/fdt_network_matrix"
         SUBJECT_SOURCE_FILE="${DTI_DIR}/${subject_name}/sources_lang_glasser_LH_thr_${threshold}_${SOURCEJoin}_EX_${EXCLUDEJoin}.txt"
         printf "%d,%s,%s,%s,%s,%s\n" "$LINE_COUNT" "$subject_name" "LH" "$lh_tr_file" "$SUBJECT_SOURCE_FILE" "$lh_file" >> $SUBJECT_PROBX_FILE
-
+      else
+        if [ ! -f "$lh_file" ]
+          then
+            echo "missing ${lh_file}"
+            LINE_COUNT=$(expr ${LINE_COUNT} + 1)
+            # folder to find the file
+            lh_tr_file="${DTI_DIR}/${subject_name}/dti.probtrackx/lang_glasser_LH_thr_${threshold}_${SOURCEJoin}_TO_${TARGETSJoin}_EX_${EXCLUDEJoin}/fdt_network_matrix"
+            SUBJECT_SOURCE_FILE="${DTI_DIR}/${subject_name}/sources_lang_glasser_LH_thr_${threshold}_${SOURCEJoin}_EX_${EXCLUDEJoin}.txt"
+            printf "%d,%s,%s,%s,%s,%s\n" "$LINE_COUNT" "$subject_name" "LH" "$lh_tr_file" "$SUBJECT_SOURCE_FILE" "$lh_file" >> $SUBJECT_PROBX_FILE
+        fi
       fi
-      #if [ ! -f "$rh_file" ]
-      if [ true ]
+
+      if [ "$overwrite" = true ]
       then
+        echo "overwriting ${rh_file}"
         LINE_COUNT=$(expr ${LINE_COUNT} + 1)
         rh_tr_file="${DTI_DIR}/${subject_name}/dti.probtrackx/lang_glasser_RH_thr_${threshold}_${SOURCEJoin}_TO_${TARGETSJoin}_EX_${EXCLUDEJoin}/fdt_network_matrix"
         SUBJECT_SOURCE_FILE="${DTI_DIR}/${subject_name}/sources_lang_glasser_RH_thr_${threshold}_${SOURCEJoin}_EX_${EXCLUDEJoin}.txt"
         printf "%d,%s,%s,%s,%s,%s\n" "$LINE_COUNT" "$subject_name" "RH" "$rh_tr_file" "$SUBJECT_SOURCE_FILE" "$rh_file" >> $SUBJECT_PROBX_FILE
+      else
+        if [ ! -f "$rh_file" ]
+        then
+          echo "missing ${rh_file}"
+          LINE_COUNT=$(expr ${LINE_COUNT} + 1)
+          rh_tr_file="${DTI_DIR}/${subject_name}/dti.probtrackx/lang_glasser_RH_thr_${threshold}_${SOURCEJoin}_TO_${TARGETSJoin}_EX_${EXCLUDEJoin}/fdt_network_matrix"
+          SUBJECT_SOURCE_FILE="${DTI_DIR}/${subject_name}/sources_lang_glasser_RH_thr_${threshold}_${SOURCEJoin}_EX_${EXCLUDEJoin}.txt"
+          printf "%d,%s,%s,%s,%s,%s\n" "$LINE_COUNT" "$subject_name" "RH" "$rh_tr_file" "$SUBJECT_SOURCE_FILE" "$rh_file" >> $SUBJECT_PROBX_FILE
+        fi
       fi
 done < <(find $DTI_DIR -maxdepth 1 -type d -name "sub*")
 
+echo $LINE_COUNT
 run_val=0
 if [ "$LINE_COUNT" -gt "$run_val" ]; then
   echo "running  ${LINE_COUNT} jobs"
-   #nohup /cm/shared/admin/bin/submit-many-jobs 3 2 3 1 collect_probtrackX_result_on_select_tracts.sh  $SUBJECT_PROBX_FILE &
-   nohup /cm/shared/admin/bin/submit-many-jobs $LINE_COUNT 75 100 25 collect_probtrackX_result_on_select_tracts.sh  $SUBJECT_PROBX_FILE &
+  if [ "$LINE_COUNT" -lt 100 ] ; then
+    echo "less than 100 jobs:  ${LINE_COUNT} jobs"
+    nohup /cm/shared/admin/bin/submit-many-jobs $LINE_COUNT "$LINE_COUNT" "$LINE_COUNT" 0 collect_probtrackX_result_on_select_tracts.sh  $SUBJECT_PROBX_FILE
+    else
+      echo "more than 100 jobs:  ${LINE_COUNT} jobs"
+      #nohup /cm/shared/admin/bin/submit-many-jobs 3 2 3 1 collect_probtrackX_result_on_select_tracts.sh  $SUBJECT_PROBX_FILE &
+      nohup /cm/shared/admin/bin/submit-many-jobs $LINE_COUNT 75 100 25 collect_probtrackX_result_on_select_tracts.sh  $SUBJECT_PROBX_FILE &
+  fi
   else
     echo $LINE_COUNT
 fi
