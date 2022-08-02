1eaf19bb79c306ac914013a469c6a124b5561394
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-08-01 21:55:19 -0400

    running anttemp

--- bash/run_collect_probtrackX_result_on_select_tracts.sh
+++ bash/run_collect_probtrackX_result_on_select_tracts.sh
@@ -10,18 +10,18 @@ rm -f $SUBJECT_PROBX_FILE
 touch $SUBJECT_PROBX_FILE
 
 
-#SOURCES=("IFGorb_top_${threshold}" "AntTemp_top_${threshold}")
-#TARGETS=("IFGorb_top_${threshold}" "AntTemp_top_${threshold}")
+SOURCES=("IFGorb_top_${threshold}" "AntTemp_top_${threshold}")
+TARGETS=("IFGorb_top_${threshold}" "AntTemp_top_${threshold}")
 #EXCLUDES=("MFG_top_${threshold}" "IFG_top_${threshold}")
-#EXCLUDES=("MFG_top_${threshold}")
+EXCLUDES=("MFG_top_${threshold}")
 #EXCLUDES=("IFG_top_${threshold}")
 
 
-SOURCES=("IFG_top_${threshold}" "PostTemp_top_${threshold}")
-TARGETS=("IFG_top_${threshold}" "PostTemp_top_${threshold}")
+#SOURCES=("IFG_top_${threshold}" "PostTemp_top_${threshold}")
+#TARGETS=("IFG_top_${threshold}" "PostTemp_top_${threshold}")
 #EXCLUDES=("MFG_top_${threshold}" "IFGorb_top_${threshold}")
 #EXCLUDES=("IFGorb_top_${threshold}")
-EXCLUDES=("MFG_top_${threshold}")
+#EXCLUDES=("MFG_top_${threshold}")
 SOURCEJoin=$(IFS=- ; echo "${SOURCES[*]}")
 TARGETSJoin=$(IFS=- ; echo "${TARGETS[*]}")
 EXCLUDEJoin=$(IFS=- ; echo "${EXCLUDES[*]}")
