d0286f3554a16389bce32c38bd389416ca658d74
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-08-07 14:23:42 -0400

    bugfix

--- bash/run_collect_probtrackX_result_on_all_subjects.sh
+++ bash/run_collect_probtrackX_result_on_all_subjects.sh
@@ -54,8 +54,14 @@ done < <(find $DTI_DIR -maxdepth 1 -type d -name "sub*")
 run_val=0
 if [ "$LINE_COUNT" -gt "$run_val" ]; then
   echo "running  ${LINE_COUNT} jobs"
-   #nohup /cm/shared/admin/bin/submit-many-jobs 3 2 3 1 collect_probtrackX_result_on_subject.sh  $SUBJECT_PROBX_FILE
-   nohup /cm/shared/admin/bin/submit-many-jobs $LINE_COUNT 75 100 25 collect_probtrackX_result_on_subject.sh  $SUBJECT_PROBX_FILE &
+  if [ "$LINE_COUNT" -lt 100 ] ; then
+    echo "less than 100 jobs:  ${LINE_COUNT} jobs"
+    nohup /cm/shared/admin/bin/submit-many-jobs $LINE_COUNT "$LINE_COUNT" "$LINE_COUNT" 0 collect_probtrackX_result_on_subject.sh  $SUBJECT_PROBX_FILE
+  else
+    echo "more than 100 jobs:  ${LINE_COUNT} jobs"
+    #nohup /cm/shared/admin/bin/submit-many-jobs 3 2 3 1 collect_probtrackX_result_on_subject.sh  $SUBJECT_PROBX_FILE
+    nohup /cm/shared/admin/bin/submit-many-jobs $LINE_COUNT 75 100 25 collect_probtrackX_result_on_subject.sh  $SUBJECT_PROBX_FILE &
+  fi
   else
     echo $LINE_COUNT
 fi
