c71bde8b87ee35dd4bffbd65d12e8a04587668e8
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-08-08 17:38:34 -0400

    avoid instead of stop

--- bash/run_probtrackX_on_select_tracts.sh
+++ bash/run_probtrackX_on_select_tracts.sh
@@ -56,8 +56,8 @@ LINE_COUNT=0
 SOURCES=("IFG_top_${threshold}" "LH_AngG_top_${threshold}")
 TARGETS=("IFG_top_${threshold}" "LH_AngG_top_${threshold}")
 #EXCLUDES=("IFGorb_top_${threshold}" "MFG_top_${threshold}")
-EXCLUDES=("IFGorb_top_${threshold}")
-#EXCLUDES=("MFG_top_${threshold}")
+#EXCLUDES=("IFGorb_top_${threshold}")
+EXCLUDES=("MFG_top_${threshold}")
 
 #SOURCES=("MFG_top_${threshold}" "LH_AngG_top_${threshold}")
 #TARGETS=("MFG_top_${threshold}" "LH_AngG_top_${threshold}")
