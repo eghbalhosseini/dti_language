018fa442b7f9d8f212fc356ccdee21f76931ed6e
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-08-08 19:25:49 -0400

    bugfix

--- bash/run_collect_probtrackX_result_on_select_tracts.sh
+++ bash/run_collect_probtrackX_result_on_select_tracts.sh
@@ -19,13 +19,13 @@ touch $SUBJECT_PROBX_FILE
 #EXCLUDES=("IFG_top_${threshold}" "MFG_top_${threshold}")
 
 
-SOURCES=("IFG_top_${threshold}" "AngG_top_${threshold}")
-TARGETS=("IFG_top_${threshold}" "AngG_top_${threshold}")
-EXCLUDES=("IFGorb_top_${threshold}")
+#SOURCES=("IFG_top_${threshold}" "AngG_top_${threshold}")
+#TARGETS=("IFG_top_${threshold}" "AngG_top_${threshold}")
+#EXCLUDES=("IFGorb_top_${threshold}")
 
-#SOURCES=("MFG_top_${threshold}" "AngG_top_${threshold}")
-#TARGETS=("MFG_top_${threshold}" "AngG_top_${threshold}")
-#EXCLUDES=("IFG_top_${threshold}")
+SOURCES=("MFG_top_${threshold}" "AngG_top_${threshold}")
+TARGETS=("MFG_top_${threshold}" "AngG_top_${threshold}")
+EXCLUDES=("IFG_top_${threshold}")
 
 SOURCEJoin=$(IFS=- ; echo "${SOURCES[*]}")
 TARGETSJoin=$(IFS=- ; echo "${TARGETS[*]}")
