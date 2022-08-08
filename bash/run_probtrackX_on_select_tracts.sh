4cd3b7615aac6cdf008aec42dc9047cd5ef2f820
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-08-08 18:06:04 -0400

    bugfix

--- bash/run_probtrackX_on_select_tracts.sh
+++ bash/run_probtrackX_on_select_tracts.sh
@@ -47,20 +47,20 @@ LINE_COUNT=0
 #EXCLUDES=("IFG_top_${threshold}")
 
 # AngG Targets
-#SOURCES=("IFGorb_top_${threshold}" "LH_AngG_top_${threshold}")
-#TARGETS=("IFGorb_top_${threshold}" "LH_AngG_top_${threshold}")
+#SOURCES=("IFGorb_top_${threshold}" "AngG_top_${threshold}")
+#TARGETS=("IFGorb_top_${threshold}" "AngG_top_${threshold}")
 #EXCLUDES=("IFG_top_${threshold}" "MFG_top_${threshold}")
 #EXCLUDES=("IFG_top_${threshold}")
 #EXCLUDES=("MFG_top_${threshold}")
 
-#SOURCES=("IFG_top_${threshold}" "LH_AngG_top_${threshold}")
-#TARGETS=("IFG_top_${threshold}" "LH_AngG_top_${threshold}")
+#SOURCES=("IFG_top_${threshold}" "AngG_top_${threshold}")
+#TARGETS=("IFG_top_${threshold}" "AngG_top_${threshold}")
 #EXCLUDES=("IFGorb_top_${threshold}" "MFG_top_${threshold}")
 #EXCLUDES=("IFGorb_top_${threshold}")
 #EXCLUDES=("MFG_top_${threshold}")
 
-SOURCES=("MFG_top_${threshold}" "LH_AngG_top_${threshold}")
-TARGETS=("MFG_top_${threshold}" "LH_AngG_top_${threshold}")
+SOURCES=("MFG_top_${threshold}" "AngG_top_${threshold}")
+TARGETS=("MFG_top_${threshold}" "AngG_top_${threshold}")
 #EXCLUDES=("IFGorb_top_${threshold}" "IFG_top_${threshold}")
 EXCLUDES=("IFGorb_top_${threshold}")
 #EXCLUDES=("IFG_top_${threshold}")
