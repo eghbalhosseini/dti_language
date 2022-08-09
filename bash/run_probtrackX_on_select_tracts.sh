de743bf91687cc70f5ae5c5c70b4c97eae9ca682
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-08-08 22:29:06 -0400

    bugfix

--- bash/run_probtrackX_on_select_tracts.sh
+++ bash/run_probtrackX_on_select_tracts.sh
@@ -18,15 +18,15 @@ LINE_COUNT=0
 #EXCLUDES=("IFGorb_top_${threshold}" "MFG_top_${threshold}")
 
 
-SOURCES=("MFG_top_${threshold}" "AntTemp_top_${threshold}")
-TARGETS=("MFG_top_${threshold}" "AntTemp_top_${threshold}")
-EXCLUDES=("IFG_top_${threshold}" "IFGorb_top_${threshold}")
+#SOURCES=("MFG_top_${threshold}" "AntTemp_top_${threshold}")
+#TARGETS=("MFG_top_${threshold}" "AntTemp_top_${threshold}")
+#EXCLUDES=("IFG_top_${threshold}" "IFGorb_top_${threshold}")
 
 ####################################
 # PostTemp Targets
-#SOURCES=("IFGorb_top_${threshold}" "PostTemp_top_${threshold}")
-#TARGETS=("IFGorb_top_${threshold}" "PostTemp_top_${threshold}")
-#EXCLUDES=("IFG_top_${threshold}" "MFG_top_${threshold}")
+SOURCES=("IFGorb_top_${threshold}" "PostTemp_top_${threshold}")
+TARGETS=("IFGorb_top_${threshold}" "PostTemp_top_${threshold}")
+EXCLUDES=("IFG_top_${threshold}" "MFG_top_${threshold}")
 
 #SOURCES=("IFG_top_${threshold}" "PostTemp_top_${threshold}")
 #TARGETS=("IFG_top_${threshold}" "PostTemp_top_${threshold}")
