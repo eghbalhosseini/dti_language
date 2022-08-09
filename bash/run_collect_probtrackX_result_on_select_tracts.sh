ce6f76098fc7ee7278260f9c941008a372d3ecb2
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-08-08 20:25:20 -0400

    bugfix

--- bash/run_collect_probtrackX_result_on_select_tracts.sh
+++ bash/run_collect_probtrackX_result_on_select_tracts.sh
@@ -14,9 +14,9 @@ touch $SUBJECT_PROBX_FILE
 
 #####################################
 # AntTemp Targets
-SOURCES=("IFGorb_top_${threshold}" "AntTemp_top_${threshold}")
-TARGETS=("IFGorb_top_${threshold}" "AntTemp_top_${threshold}")
-EXCLUDES=("IFG_top_${threshold}" "MFG_top_${threshold}")
+#SOURCES=("IFGorb_top_${threshold}" "AntTemp_top_${threshold}")
+#TARGETS=("IFGorb_top_${threshold}" "AntTemp_top_${threshold}")
+#EXCLUDES=("IFG_top_${threshold}" "MFG_top_${threshold}")
 
 #SOURCES=("IFG_top_${threshold}" "AntTemp_top_${threshold}")
 #TARGETS=("IFG_top_${threshold}" "AntTemp_top_${threshold}")
